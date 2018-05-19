b3b65fcf1a7199e4cdb61fb4e8b4917511198f9f
Manoj <manoj.bharadwaj@sandbox.loca>
2018-05-19 14:49:20 +0100

    added semantic to storybook

--- Client/src/jsx/components/custom/spokersubviews/SelectPointCard.jsx
+++ Client/src/jsx/components/custom/spokersubviews/SelectPointCard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Card, Image } from 'semantic-ui-react'
+import { Card, Image } from 'semantic-ui-react';
+
 // import CardImg from '../../../../img/jpg/Penguins.jpg';
 class SelectPointCard extends Component {
     constructor(props) {
@@ -12,8 +13,9 @@ class SelectPointCard extends Component {
 
     render() {
         return (
-            <Card id={this.props.point} style={{ margin: 10, cursor: 'pointer' }}  onClick={this.handleClick}>
-                <Image  size="small" src={this.props.image} />
+            
+            <Card fluid id={this.props.point} style={{maxHeight:'100%'}}   onClick={this.handleClick}>
+                <Image fluid src={this.props.image} />
                 <Card.Content style={{position:"absolute", left: '50%', top:'70%',
                 transform: 'translate(-50%, -50%)', border: 'none', fontSize: '3vm' , color: 'black', }}>
                 {this.props.point}
--- Client/stories/index.js
+++ Client/stories/index.js
@@ -4,22 +4,24 @@ import { action } from '@storybook/addon-actions';
 import SelectPointCard from '../src/jsx/components/custom/spokersubviews/SelectPointCard.jsx'
 import selectVote from '../src/img/jpg/angry.jpg';
 let actions = {
-    selectPoint:(obj)=>{action(obj.toString())}
+    selectPoint: (obj) => { action('clicked')(obj) }
 };
-// storiesOf('Button', module)
-//   .add('with text', () => (
-//     <Button onClick={action('clicked')}>Hello Button</Button>
-//   ))
-//   .add('with some emoji', () => (
-//     <Button onClick={action('clicked')}><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
-//   ));   
-storiesOf('SelectPointCard',module)
-  .add('initial',()=>(
-    <SelectPointCard point={1} image={selectVote} actions={actions}/>
-  ))
-  .add('selected',()=>(
-    <SelectPointCard point={1} image={selectVote} actions={actions}/>
-  ))
-  .add('notSelected',()=>(
-    <SelectPointCard point={1} image={selectVote} actions={actions}/>
-  ));
+ 
+storiesOf('SelectPointCard', module)
+    .addDecorator(story => (
+        <div style={{ width: '300px', height: '400px', border: '2px solid red', position: 'relative' }}>
+            <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+
+            {story()}
+        </div>
+    ))
+    .add('initial', () => (
+
+        <SelectPointCard point={1} image={selectVote} actions={actions} />
+    ))
+    .add('selected', () => (
+        <SelectPointCard point={1} image={selectVote} actions={actions} />
+    ))
+    .add('notSelected', () => (
+        <SelectPointCard point={1} image={selectVote} actions={actions} />
+    ));
