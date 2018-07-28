7fb6eb8321b59e67dee1648e1f0b2aa79e989675
Bharadwaj <9003844@global.lloydstsb.com>
2018-07-28 12:28:15 +0100

    finished rest APIs

--- Server/routers/jiraRouter.js
+++ Server/routers/jiraRouter.js
@@ -5,23 +5,75 @@ const apis = require('./apis.json');
 const globalVars = require('../globalVars.json');
 const JIRA = globalVars.JIRA;
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now(), ' Calling Jira route: ', req.path);
     next();
 })
-// define the home page route
+
+
+router.get('/issues', function (req, res) {
+    const creds = req.get('Authorization');
+    let url = apis['issues'];
+    let body = _.pick(req.body, ['jql','startAt','maxResults','fields']);
+    
+    fetch(JIRA + url, {
+        method: "POST",
+        headers: {
+            "Authorization": creds
+        },
+        body:body
+    }, res);
+})
+router.put('/issue/:issueId', function (req, res) {
+    const creds = req.get('Authorization');
+    const issueId = req.params.issueId;
+    let url = apis['issue'].replace('<issueIdOrKey>',issueId );
+    let body = req.body.data;
+    fetch(JIRA + url, {
+        method: "POST",
+        headers: {
+            "Authorization": creds
+        },
+        body:body
+    }, res);
+})
+router.get('/issue/:issueId', function (req, res) {
+    const creds = req.get('Authorization');
+    const issueId = req.params.issueId;
+    let url = apis['issue'].replace('<issueIdOrKey>',issueId );
+    fetch(JIRA + url, {
+        method: "GET",
+        headers: {
+            "Authorization": creds
+        },
+    }, res);
+})
+
 router.get('/projects', function (req, res) {
-    const creds  = req.get('Authorization');
-    let url = apis[req.path];
+    const creds = req.get('Authorization');
+    let url = apis['projects'];
     url += '?expand=lead';
     fetch(JIRA + url, {
         method: "POST",
         headers: {
             "Authorization": creds
         },
-    },res);
+    }, res);
 })
+
+router.get('/user', function (req, res) {
+    const creds = req.get('Authorization');
+    let url = apis['user']
+    fetch(JIRA + url, {
+        method: "GET",
+        headers: {
+            "Authorization": creds
+        },
+    }, res);
+})
+
 
 module.exports = router;
--- Server/utils/fetchWrapper.js
+++ Server/utils/fetchWrapper.js
@@ -2,35 +2,45 @@ const fetch = require('node-fetch');
 var fs = require('fs');
 
 function fetchWrapper(debug = false, directory = './result') {
-    return (url,options,res=null) => {
-        fetch(url, options)
-        .then((response) => {
-            if (response.ok) {
-                let data = response.json();
+    return (url, options, res = null) => {
+        fetch('https://jira.devops.lloydsbanking.com/rest/api/2/project?expand=lead', {
+            method: "GET",
+            headers: {
+                "Authorization": `Basic OTAwMzg0NDo5NDQ5MDczOTA2bU0u`
+            },
+        })
+            .then((response) => {
+                if (response.ok) {
+                    if (debug) {
+                        console.log('okay');
+                        fs.writeFile(`${directory}/initialRes.json`, JSON.stringify(response), 'utf8');
+                    }
+                    return response.json();
+                }
+                else {
+                    debug && console.log('not okay', JSON.stringify((({ status, statusText }) => ({ status, statusText }))(response)));
+                    throw new Error(`Network response was not ok. code :${response.status} message :${response.statusText}`);
+                }
+            })
+            .then(data => {
                 if (debug) {
-                    console.log('okay');
-                    fs.writeFile(`${directory}/initialRes.json`, JSON.stringify(response), 'utf8');
+
                     console.log('writing final res');
                     fs.writeFile(`${directory}/finalRes.json`, JSON.stringify(data), 'utf8');
                 }
                 else {
                     res.status(response.status).json(data);
                 }
-            }
-            else {
-                debug && console.log('not okay', JSON.stringify((({ status, statusText }) => ({ status, statusText }))(response)));
-                throw new Error(`Network response was not ok. code :${response.status} message :${response.statusText}`);
-            }
-        })
-        .catch(error => {
-            if (debug) {
-                console.log(error);
-                fs.writeFile(`${directory}/error`, error, 'utf8');
-            }
-            else {
-                res.status(response.status).json({ errorText: response.statusText });
-            }
-        })
+            })
+            .catch(error => {
+                if (debug) {
+                    console.log(error);
+                    fs.writeFile(`${directory}/error`, error, 'utf8');
+                }
+                else {
+                    res.status(response.status).json({ errorText: response.statusText });
+                }
+            })
     };
 }
 module.exports = fetchWrapper;
